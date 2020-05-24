4ac84b8f7f815fee62fa1122fda0d4b4b4caf9f9
Jan Timar <jantimar2@gmail.com>
2020-05-24 12:05:55 +0200

    Add MonthView

---
+++ Sources/CalendarView/MonthView.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+
+struct MonthView: View {
+    let weeks: [Week]
+
+    var body: some View {
+        VStack {
+            ForEach(weeks, content: WeekView.init(week:))
+        }
+    }
+}
+
+struct MonthView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        var weeks = [Week]()
+        for week in 0...4 {
+            var days = [Day]()
+            for day in 1...7 {
+                days.append(Day(number: week * 7 + day))
+            }
+            weeks.append(Week(days: days))
+        }
+
+        return MonthView(weeks: weeks)
+            .previewLayout(.sizeThatFits)
+    }
+}
