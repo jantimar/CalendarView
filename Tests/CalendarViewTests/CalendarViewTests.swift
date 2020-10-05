b76967329afb704b532815c08ec5e4a923ace0c8
Jan Timar <timar@cngroup.dk>
2020-10-05 21:10:35 +0200

    Update tests

--- Tests/CalendarViewTests/CalendarViewTests.swift
+++ Tests/CalendarViewTests/CalendarViewTests.swift
@@ -2,14 +2,24 @@ import XCTest
 @testable import CalendarView
 
 final class CalendarViewTests: XCTestCase {
-//    func testExample() {
-//        // This is an example of a functional test case.
-//        // Use XCTAssert and related functions to verify your tests produce the correct
-//        // results.
-////        XCTAssertEqual(CalendarView().text, "Hello, World!")
-//    }
-//
-//    static var allTests = [
-////        ("testExample", testExample),
-//    ]
+
+    static var allTests = [
+        ("testGetWeek", testGetWeek),
+    ]
+
+
+    func testGetWeek() {
+        let calendarViewModel = CalendarViewModel(
+            today: Date(timeIntervalSince1970: 0),
+            heighlighted: [],
+            selectedDate: nil
+        )
+
+        // Get second week of week ( first is 0 )
+        let week = calendarViewModel.week(of: 1)
+
+        XCTAssertNotNil(week)
+        XCTAssertEqual(week?.days.count, 7)
+        XCTAssertEqual(week?.days.first?.number, 4)
+    }
 }
